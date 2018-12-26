2470a187458e4414870a0681ebde00503edcd179
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2018-12-26 18:26:45 -0200

    Check bombas

--- src/Grid.js
+++ src/Grid.js
@@ -19,7 +19,53 @@ class Grid extends Component{
             grid = grid.concat(0);
         }
         random.forEach((v) => {
-            grid[v] = 1;
+            console.log(v)
+            grid[v] = 9;
+            if(v % 10 === 0){
+                grid[v+1] += grid[v+1] !== 9 ? 1 : 0;
+                grid[v+11] += grid[v+11] !== 9 ? 1 : 0;
+                grid[v+10] += grid[v+10] !== 9 ? 1 : 0;
+                grid[v-9] += grid[v-9] !== 9 ? 1 : 0;
+                grid[v-10] += grid[v-10] !== 9 ? 1 : 0;
+                //vizinhos [v + 1], [v+11], [v+10], [v-9], [v-10]
+            }else{
+                if(v % 10 === 9){
+                    grid[v-1] += grid[v-1] !== 9 ? 1 : 0;
+                    grid[v-10] += grid[v-10] !== 9 ? 1 : 0;
+                    grid[v-11] += grid[v-11] !== 9 ? 1 : 0;
+                    grid[v+9] += grid[v+9] !== 9 ? 1 : 0;
+                    grid[v+10] += grid[v+10] !== 9 ? 1 : 0;
+                    //vizinhos [v - 1], [v - 10], [v - 11], [v + 9], [v+10] 
+                }else{
+                    if(v < 10){
+                        grid[v-1] += grid[v-1] !== 9 ? 1 : 0;
+                        grid[v+1] += grid[v+1] !== 9 ? 1 : 0;
+                        grid[v+9] += grid[v+9] !== 9 ? 1 : 0;
+                        grid[v+10] += grid[v+10] !== 9 ? 1 : 0;
+                        grid[v+11] += grid[v+11] !== 9 ? 1 : 0;
+                        //vizinhos [v-1], [v+1], [v+9], [v+10], [v+11]
+                    }else{
+                        if(v >= 90){
+                            grid[v-1] += grid[v-1] !== 9 ? 1 : 0;
+                            grid[v+1] += grid[v+1] !== 9 ? 1 : 0;
+                            grid[v-9] += grid[v-9] !== 9 ? 1 : 0;
+                            grid[v-10] += grid[v-10] !== 9 ? 1 : 0;
+                            grid[v-11] += grid[v-11] !== 9 ? 1 : 0;
+                            //vizinhos [v-1], [v+1], [v-9], [v-10], [v-11]
+                        }else{
+                            grid[v-1] += grid[v-1] !== 9 ? 1 : 0;
+                            grid[v+1] += grid[v+1] !== 9 ? 1 : 0;
+                            grid[v-9] += grid[v-9] !== 9 ? 1 : 0;
+                            grid[v+9] += grid[v+9] !== 9 ? 1 : 0;
+                            grid[v-10] += grid[v-10] !== 9 ? 1 : 0;
+                            grid[v+10] += grid[v+10] !== 9 ? 1 : 0;
+                            grid[v-11] += grid[v-11] !== 9 ? 1 : 0;
+                            grid[v+11] += grid[v+11] !== 9 ? 1 : 0;
+                        }
+                    }
+                }
+            }
+            
         })
         this.setState({
             grid: grid
@@ -29,12 +75,12 @@ class Grid extends Component{
     renderGrid(){
         return this.state.grid.map((value, index) => {
             if(index === 0){
-                return <div className="grid">{value}</div>
+                return <div className="grid" style={{backgroundColor: value === 9 ? 'red' : 'white'}}>{value}</div>
             }else{
                 if(index % 10 !== 9){
-                    return <div className="grid">{value}</div>
+                    return <div className="grid" style={{backgroundColor: value === 9 ? 'red' : 'white'}}>{value}</div>
                 }else{
-                    return <span><div className="grid">{value}</div><br /></span>
+                    return <span><div className="grid" style={{backgroundColor: value === 9 ? 'red' : 'white'}} >{value}</div><br /></span>
                 }
             }
         })
