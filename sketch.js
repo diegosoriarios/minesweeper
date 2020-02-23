2fba42cb6cd6cd68ddbf4bc2aafe786a5c928e70
diegosoriarios <diegosoriarios@gmail.com>
2020-02-23 19:24:21 -0300

    Full Code

---
+++ sketch.js
@@ -0,0 +1,92 @@
+function make2DArray(cols, rows) {
+    var arr = new Array(cols);
+    
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+
+    return arr;
+}
+
+var grid;
+var cols;
+var rows;
+var w = 20;
+var totalMine = 10;
+
+function setup() {
+    createCanvas(201, 201);
+    cols = floor(width / w);
+    rows = floor(height / w);
+
+    grid = make2DArray(cols, rows);
+
+    for(var i = 0; i < cols; i++) {
+        for(var j = 0; j < rows; j++) {
+            grid[i][j] = new Cell(i, j, w);
+        }
+    }
+
+    var options = [];
+    for (var i = 0; i < cols; i++){
+        for (var j = 0; j < rows; j++) {
+            options.push([i, j]);
+        }
+    }
+
+    for (var n = 0; n < totalMine; n++) {
+        var index = floor(random(options.length));
+        var choice = options[index]
+        var i = choice[0];
+        var j = choice[1];
+        options.splice(index, 1)
+        grid[i][j].mine = true;
+    }
+
+    for(var i = 0; i < cols; i++) {
+        for(var j = 0; j < rows; j++) {
+            grid[i][j].countMines();
+        }
+    }
+}
+
+function mousePressed() { 
+    for(var i = 0; i < cols; i++) {
+        for(var j = 0; j < rows; j++) {
+            if (mouseButton === LEFT) {
+                if (grid[i][j].contains(mouseX, mouseY) && !grid[i][j].flag) {
+                    grid[i][j].reveal();
+
+                    if (grid[i][j].mine) {
+                        gameOver();
+                    }
+                }
+            }
+            if (mouseButton === RIGHT) {
+
+                if (grid[i][j].contains(mouseX, mouseY) && !grid[i][j].revealed) {
+                    grid[i][j].flag = !grid[i][j].flag;
+                }
+            }
+        }
+    }
+}
+
+function draw() {
+    background(255);
+
+    for(var i = 0; i < cols; i++) {
+        for(var j = 0; j < rows; j++) {
+            grid[i][j].show();
+        }
+    }
+}
+
+function gameOver(){
+    for(var i = 0; i < cols; i++) {
+        for(var j = 0; j < rows; j++) {
+            grid[i][j].flag = false;
+            grid[i][j].revealed = true;
+        }
+    }
+}
